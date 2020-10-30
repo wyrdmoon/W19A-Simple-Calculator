ce762f4d7482611b81dfbe782f9aea219ba84959
wyrdmoon <criscorcoran@gmail.com>
2020-10-29 21:02:42 -0600

    started calculator

---
+++ app.py
@@ -0,0 +1,14 @@
+import addition.py
+import subtraction.py
+import multiplication.py
+import division.py 
+
+print ("choose an operation")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+userChoice = input ("Enter 1 or 2 or 3 or 4 : ")
+if userChoice in ('1', '2', '3', '4' ):
+    
